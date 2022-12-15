ee9d71a0a64f233854c56a10280d885afa3156c2
AlexisCasTawil <alexis_a_k@hotmail.com>
2022-12-15 15:33:03 -0500

    Improving MovieFilter component

--- src/modules/core/components/molecules/Filter/MovieFilter.jsx
+++ src/modules/core/components/molecules/Filter/MovieFilter.jsx
@@ -1,15 +1,53 @@
 import React, { useState } from 'react';
 import './styles.scss';
 import { RiArrowDropDownFill } from 'react-icons/ri';
-import MovieList from './MovieList';
 
 export default function MovieFilter() {
   const [toggle, setToggle] = useState(false);
+  const [checked, setChecked] = useState([]);
+
+  const handleCheck = (e) => {
+    let updatedList = [...checked];
+    if (e.target.checked) {
+      updatedList = [...checked, e.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(e.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  //Función para el contenedor de películas
+  function MovieList() {
+    return (
+      <div className="movies">
+        <ul className="movies__list">
+          {movies.map((item, index) => (
+            <li key={index} className="movies__list--item">
+              <label className="movies__list--text">
+                <input value={item} type="checkbox" onChange={handleCheck} />
+                {item}
+              </label>
+            </li>
+          ))}
+        </ul>
+        <div className="movies__buttons--container">
+          <div className="movies__buttons--left">
+            <button className="movies__buttons">Apply</button>
+          </div>
+          <div className="movies__buttons--right">
+            <button className="movies__buttons">Reset</button>
+            <button className="movies__buttons">Close</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   const handleToggle = () => {
     setToggle(!toggle);
   };
 
+  //Este es el "return" de <MovieFilter />
   return (
     <div className="filters">
       <div className="filters__movie" onClick={handleToggle}>
@@ -18,7 +56,38 @@ export default function MovieFilter() {
           className={`filters__movie icon ${toggle && 'filters__movie__icon-open'}`}
         />
       </div>
-      {toggle && <MovieList />}
+      {toggle && MovieList()}
     </div>
   );
 }
+
+const movies = [
+  'ANT-MAN',
+  'ANT-MAN AND THE WASP',
+  'BLACK PANTHER',
+  'BLACK PANTHER: WAKANDA FOREVER',
+  'ANT-MAN',
+  'ANT-MAN AND THE WASP',
+  'BLACK PANTHER',
+  'BLACK PANTHER: WAKANDA FOREVER',
+  'ANT-MAN',
+  'ANT-MAN AND THE WASP',
+  'BLACK PANTHER',
+  'BLACK PANTHER: WAKANDA FOREVER',
+  'ANT-MAN',
+  'ANT-MAN AND THE WASP',
+  'BLACK PANTHER',
+  'BLACK PANTHER: WAKANDA FOREVER',
+  'ANT-MAN',
+  'ANT-MAN AND THE WASP',
+  'BLACK PANTHER',
+  'BLACK PANTHER: WAKANDA FOREVER',
+  'ANT-MAN',
+  'ANT-MAN AND THE WASP',
+  'BLACK PANTHER',
+  'BLACK PANTHER: WAKANDA FOREVER',
+  'ANT-MAN',
+  'ANT-MAN AND THE WASP',
+  'BLACK PANTHER',
+  'BLACK PANTHER: WAKANDA FOREVER'
+];
